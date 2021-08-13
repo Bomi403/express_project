ba5f728d97c742feee405f6b938b3d27e3e56d6e
Bomi403 <qhal2364@gmail.com>
2021-08-13 18:09:23 +0900

    join2

--- middleware/validator.js
+++ middleware/validator.js
@@ -53,3 +53,20 @@ module.exports.joinValidator = async (req, res, next) => {
 	}
 	next();
 };
+
+/** 로그인 유효성 검사 */
+module.exports.loginValidator = (req, res, next) => {
+	try {
+		if (!req.body.memId) {
+			throw new Error("아이디를 입력하세요.");
+		}
+		
+		if (!req.body.memPw) {
+			throw new Error("비밀번호를 입력하세요.");
+		}
+		
+	} catch(err) {
+		return alert(err.message, res);
+	}
+	next();
+};
